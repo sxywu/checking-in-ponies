0efbc64483ebfb168347a4cec7ab6aa310704436
Shirley Wu <shirleyxueyangwu@gmail.com>
2013-08-16 22:39:08 -0700

    correct People.Model: "response", not "rsvp"

--- scripts/models/People.Model.js
+++ scripts/models/People.Model.js
@@ -12,13 +12,13 @@ define([
 	- Meetup id
 	- info: {avatar, name, bio}
 	- joined (date)
-	- rsvp (yes, no, waitlist)
+	- response (yes, no, waitlist)
 	- number of guests
 	- checkin (time, default no)
 	*/
 	return Backbone.Model.extend({
 		initialize: function() {
-			this.attributes.rsvp = this.attributes.rsvp || "no"; // default values in case 
+			this.attributes.response = this.attributes.response || "no"; // default values in case 
 			this.attributes.guests = this.attributes.guests || 0; // it's not provided
 		}
 	});
